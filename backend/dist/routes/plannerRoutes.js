059c7ed4bcd2f74015b8ebb337da76802c886625
AreebAmir <aamir.bscs18seecs@seecs.edu.pk>
2026-05-25 22:53:11 +0300

    cleaning main branch

--- backend/dist/routes/plannerRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const plannerController_1 = require("../controllers/plannerController");
-const router = (0, express_1.Router)();
-router.post("/validate", plannerController_1.validatePlannerPlan);
-// Example: POST /api/planner/validate
-exports.default = router;
--- backend/dist/routes/unitRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const courseController_1 = require("../controllers/courseController");
-const router = (0, express_1.Router)();
-router.get("/", courseController_1.getAllUnits);
-router.get("/:code", courseController_1.getUnitDetails);
-exports.default = router;
